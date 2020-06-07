0f877b27722f3f2f757961489eb0f93fe7d6234e
Elitsa <ectky3@gmail.com>
2020-06-07 17:26:10 +0300

    finished index page with responsive added procedures page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,32 @@
-window.addEventListener('load', (event) => {
+var slideIndex;
+var navbarPosition;
+var navbar;
+
+function bodyLoaded() {
   document.getElementsByTagName('body')[0].classList.add('loaded');
-});
 
-window.onscroll = function () {
-  scrollPage()
-};
+  navbar = document.getElementById("navbar");
+  navbarPosition = navbar.offsetTop;
 
-var navbar = document.getElementsByClassName("site-nav")[0];
+  if (document.title === "E • SALON - Процедури") {
+    $( "#menu" ).menu({
+      items: "> :not(.ui-widget-header)"
+    });
 
-var navbarPosition = navbar.offsetTop;
+  } else if (document.title === "E • SALON"){
+    slideIndex = 1;
+    showSlides(slideIndex);
+
+    while($(".index-f").find("h2").width() > $(".index-f").width()) {
+      var currentFontSize = parseInt($(".index-f").find("h2").css("font-size")); 
+      $(".index-f").find("h2").css("font-size",currentFontSize-1); 
+    };
+  }
+
+}
 
 function scrollPage() {
-  if (window.pageYOffset >= navbarPosition) {
+  if (Window.pageYOffset >= navbarPosition) {
     navbar.classList.add("sticky")
   } else {
     navbar.classList.remove("sticky");
@@ -33,4 +48,28 @@ function burgerMenu() {
   } else {
     x.style.display = "block";
   }
+}
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
